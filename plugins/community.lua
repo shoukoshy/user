08a08ef4ac128e0fc1e3e4a0938cc28b58bfdb58
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-02-06 21:54:16 +0700

    Add transparent background

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.color.transparent-nvim" }
+  
 }
