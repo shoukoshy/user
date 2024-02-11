08b93582710481030bcc3ced033af512d9f70dd0
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-02-11 14:44:51 +0700

    Add icon for mode text in status bar

--- icons.lua
+++ icons.lua
@@ -45,4 +45,5 @@ return {
   Selected = "❯",
   Spellcheck = "󰓆",
   TabClose = "󰅙",
+  VimModeIcon = "",
 }
--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -4,7 +4,15 @@ return {
     local status = require "astronvim.utils.status"
     opts.statusline = { -- statusline
       hl = { fg = "fg", bg = "bg" },
-      status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+      status.component.mode {
+        mode_text = {
+          padding = { left = 1, right = 1 },
+          icon = {
+            kind = "VimModeIcon", -- get icon
+            padding = { right = 1 },
+          },
+        },
+      }, -- add the mode text
       status.component.git_branch(),
       status.component.file_info { filetype = {}, filename = false, file_modified = false },
       status.component.git_diff(),
