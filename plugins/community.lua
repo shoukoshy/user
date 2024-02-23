795cce4db7a4b285681cd5d1c0adb07f4ea0e707
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-02-23 22:47:35 +0700

    Add useful plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,4 +10,11 @@ return {
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.suda-vim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.hypersonic-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
 }
