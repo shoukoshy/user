b79c1da5a8d0ac466876ccf5f013d12be2cb73ba
Trần Quang Mạnh <manhtq0508@gmail.com>
2024-02-23 22:06:33 +0700

    Enable transparent for onedark theme

--- plugins/onedark.lua
+++ plugins/onedark.lua
@@ -81,7 +81,7 @@ return {
 
     options = {
       cursorline = true, -- Use cursorline highlighting?
-      transparency = false, -- Use a transparent background?
+      transparency = true, -- Use a transparent background?
       terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
       lualine_transparency = true, -- Center bar transparency?
       highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
